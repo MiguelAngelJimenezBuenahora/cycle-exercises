526f8f3f195e29933b44ed2812a18e4899d9b9a7
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-11-01 06:58:52 -0500

    Solución del ejercicio "Potencia de dos"

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-#Escriba un programa que muestre la tabla de multiplicar del 1 al 10 del número ingresado por el usuario:
+#Escriba un programa que genere todas las potencias de 2, desde la 0-ésima hasta la ingresada por el usuario:
 
-#Ingrese un numero: 9
-#9 x 1 = 9
-#9 x 2 = 18
-#9 x 3 = 27
-#9 x 4 = 36
-#9 x 5 = 45
-#9 x 6 = 54
-#9 x 7 = 63
-#9 x 8 = 72
-#9 x 9 = 81
-#9 x 10 = 90
+#Ingrese num: 10
+#1 2 4 8 16 32 64 128 256 512 1024
 
-# Función para mostrar la tabla de multiplicar
-def multiplyTable(number):
-    i = 1
-    print(f"Tabla de multiplicar del {number}:")
-    while i <= 10:
-        result = number * i
-        print(f"{number} x {i} = {result}")
-        i += 1
-
-# Solicitar al usuario que ingrese un número
-number = int(input(""""In this program ,your insert a number and i say you the multipler table from 1 to 10
-    Please inser any number: """))
-multiplyTable(number)
-
-
+import math
+number = int(input(""""In this exercise your inset a number and i show you the multiply of  from 0 to the number do your insert
+Please insert the number:"""))
+i =0
+while i <= number:
+    result = math.pow(2,i)
+    print(result)
+    i+= 1
