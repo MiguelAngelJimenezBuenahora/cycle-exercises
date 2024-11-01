91844bf46a74b58a8f0f62f860f366373e08c3ce
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-11-01 07:30:56 -0500

    Solución del ejercicio "Tabla de multiplicar"

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-#Escriba un programa que pida al usuario dos números enteros, y luego entregue la suma de todos los números que están entre ellos. Por ejemplo, si los números son 1 y 7, debe entregar como resultado 2 + 3 + 4 + 5 + 6 = 20.
+#Escriba un programa que muestre una tabla de multiplicar como la siguiente:
 
-#Ingrese num: 1
-#Ingrese num: 7
-#La suma es 20
+# 1   2   3   4   5   6   7   8   9  10
+# 2   4   6   8  10  12  14  16  18  20
+# 3   6   9  12  15  18  21  24  27  30
+# 4   8  12  16  20  24  28  32  36  40
+# 5  10  15  20  25  30  35  40  45  50
+# 6  12  18  24  30  36  42  48  54  60
+# 7  14  21  28  35  42  49  56  63  70
+# 8  16  24  32  40  48  56  64  72  80
+# 9  18  27  36  45  54  63  72  81  90
+#10  20  30  40  50  60  70  80  90 100
 
-number1 = int(input("""In this exercise i nedd your enter two number and i show you the sum of the number that are between to two number that your insert
-Please enter the number 1: """))
-number2= int(input("Please enter the number 2: "))
+#Los números deben estar alineados a la derecha.
 
-if number1 > number2:
-    number1, number2 = number2, number1 #Re-order the numbers first from small to bigger
+def multiplyTale(size):
+    matrix = [[(i*j) for j in range(1, size +1)] for i in range(1, size +1)]
+    return matrix
+size = 10
+    
+multiplyMatrix = multiplyTale(size)
 
-sum=0   
-i=number1+1
-
-while i < number2:
-    sum += i
-    i +=1
-
-print(f"""The sum is: {sum}""")
+for fila in multiplyMatrix:
+    print('\t'.join(map(str, fila)))
